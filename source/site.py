1575cb9dbc2c7f0228eb8ca9169ed6ef06c86954
nicolas evilasio <nicolas_evilasio@hotmail.com>
2024-01-18 23:28:18 -0300

    implementação do método contar_páginas que verifica quantas páginas o link carregado tem e retorna a quantidade de páginas, caso seja maior que 1 ou retorna a string index se tiver apenas 1 página.

--- source/site.py
+++ source/site.py
@@ -12,7 +12,7 @@ class Site:
        Essa classe possui métodos para acessar o site com requests, fazer webscraping com beautiful soup e gerar um csv ou json com os dados extraídos.
 
        Atributos:
-           url (str): A URL do site.
+           __url (str): A URL do site.
 
        Métodos:
            carregar_site(): Carrega o conteúdo HTML do site usando requests e beautiful soup.
@@ -20,12 +20,13 @@ class Site:
            gerar_json(): Gera um arquivo json com os dados extraídos do site.
    """
 
-    def __init__(self, url):
+    def __init__(self, url: str):
         """Inicializa um objeto Site com a URL do site.
 
                 Parâmetros:
                     url (str): A URL do site.
         """
+        self.soup = None
         self.__url = url
 
     def carregar_site(self):
@@ -36,5 +37,30 @@ class Site:
        """
         os.system('cls')
         response = requests.get(self.__url)
-        soup = BeautifulSoup(response.text, features='html.parser')
-        return soup
+        self.soup = BeautifulSoup(response.text, features='html.parser')
+        return self.soup
+
+    def contar_paginas(self):
+        """Verifica a quantidade total de páginas no site.
+
+                Cada página tem uma tag <li> cuja class "current" tem o valor "Página X de Y"
+
+                Este método faz:
+                    1 - usa o find para localizar a tag <li> com a class "current"
+                    2 - retorna o texto dentro dessa tag usando a propriedade .text
+                    3 - usei o strip para remover os espaoços no início e no fim da string retornada
+                    4 - usei o slicer [-2:] para retornar os dois últimos caracteres da string
+                    5 - Precisei usar o strip, pois se a página for menor que 10 irá ter um espaço em branco, assim ' 9'
+                    6 - por fim, transformei em int
+
+
+                Retorna:
+                    paginas (int): Quantidade de páginas ou
+                    index (str): Site tem apenas uma página
+        """
+        try:
+            ultima_pagina = int(self.soup.find('li', class_='current').text.strip()[-2:].strip())
+        except AttributeError as e:
+            ultima_pagina = 'index'
+
+        return ultima_pagina
